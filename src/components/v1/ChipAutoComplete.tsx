cb1ed2bee83ca224deded390bb5865cd05aa1405
s-deep3k <sankhadeep.genx@gmail.com>
2024-06-11 23:32:51 +0530

    feat: getting ready for vercel deployment

--- src/components/v1/ChipAutoComplete.tsx
+++ src/components/v1/ChipAutoComplete.tsx
@@ -1,29 +1,29 @@
-import { useRef, useState } from "react";
-import SearchInput from "./SearchInput"
-import SearchList from "./SearchList"
-import Chip from "./Chip";
+// import { useRef, useState } from "react";
+// import SearchInput from "./SearchInput"
+// import SearchList from "./SearchList"
+// import Chip from "./Chip";
 
-const ChipAutoComplete = ({suggestions}:{suggestions:string[]}) => {
-    const [input, changeInput] = useState<string>('')
-    const [chips,setChips] = useState<string[]>([])
-    const chipRef = useRef(null)
-    const onInputChange = (val: string)=>{
-        changeInput(val)
-    }
-    const onChipChange = (chip: string)=>{
-      chipRef.current!.addChip(chip)
-      setChips(chipRef.current!.getChips())
-    }
-  return (
-    <>
-        <div className="bg-stone-300 rounded-3xl p-2 border border-opacity-50 border-stone-400 
-    flex w-[40%] h-[55px] min-w-[400px] gap-x-1 items-center">
-        <Chip ref={chipRef}/>
-        <SearchInput onChange={onInputChange}/>
-        </div>
-        <SearchList list={{suggestions,select:input,chips}} onSelect={onChipChange}/>
-    </>
-  )
-}
+// const ChipAutoComplete = ({suggestions}:{suggestions:string[]}) => {
+//     const [input, changeInput] = useState<string>('')
+//     const [chips,setChips] = useState<string[]>([])
+//     const chipRef = useRef(null)
+//     const onInputChange = (val: string)=>{
+//         changeInput(val)
+//     }
+//     const onChipChange = (chip: string)=>{
+//       chipRef.current!.addChip(chip)
+//       setChips(chipRef.current!.getChips())
+//     }
+//   return (
+//     <>
+//         <div className="bg-stone-300 rounded-3xl p-2 border border-opacity-50 border-stone-400 
+//     flex w-[40%] h-[55px] min-w-[400px] gap-x-1 items-center">
+//         <Chip ref={chipRef}/>
+//         <SearchInput onChange={onInputChange}/>
+//         </div>
+//         <SearchList list={{suggestions,select:input,chips}} onSelect={onChipChange}/>
+//     </>
+//   )
+// }
 
-export default ChipAutoComplete
+// export default ChipAutoComplete
